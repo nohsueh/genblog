257f0a8fc4c32b5c56ece3f0862749c4226638fa
nohsueh <nohsueh@qq.com>
2025-03-23 15:20:00 +0800

    update blog-list.tsx

--- components/blog-list.tsx
+++ components/blog-list.tsx
@@ -21,7 +21,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 
-const PAGE_SIZE = 12;
+const PAGE_SIZE = 6;
 
 interface BlogListProps {
   lang: Locale;
