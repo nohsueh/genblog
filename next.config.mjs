4b7c1833e964fb17d419e5106edbe2a7baa22b49
nohsueh <nohsueh@qq.com>
2025-03-24 14:51:16 +0800

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { i18n } from "./lib/i18n-config";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/blog",
-  i18n,
+  i18n: i18n,
   images: {
     remotePatterns: [
       {
