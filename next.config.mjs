38e4c64891a0cf6b6496260c2ff44b89ed55fb76
nohsueh <nohsueh@qq.com>
2025-03-28 21:44:11 +0800

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,8 @@ const nextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: [process.env.NEXT_PUBLIC_SEARCHLYSIS_BLOG_ROOT_DOMAIN],
+      // allowedOrigins: [process.env.NEXT_PUBLIC_SEARCHLYSIS_BLOG_ROOT_DOMAIN],
+      allowedOrigins: ["*"],
     },
   },
 };
