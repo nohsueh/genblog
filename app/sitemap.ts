adccf884bade4f69db444f44e0530cf6bd142581
nohsueh <nohsueh@qq.com>
2025-04-10 21:15:07 +0800

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,9 @@ import { listAnalyses } from "@/lib/actions";
 import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const blogUrls = (await listAnalyses()).map(
+  const blogUrls = (
+    await listAnalyses(1, 10000, { group: process.env.NEXT_PUBLIC_ROOT_DOMAIN })
+  ).map(
     (blog) => `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/blog/${blog.analysisId}`,
   );
 
@@ -10,6 +12,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/blog`,
     },
-    ...blogUrls.map(url => ({ url })),
+    ...blogUrls.map((url) => ({ url })),
   ];
 }
