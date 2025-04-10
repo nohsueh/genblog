f1e4fee2bcb03ec71a19f850cfe8dd0e5ad081dd
nohsueh <nohsueh@qq.com>
2025-04-10 23:58:10 +0800

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,17 +3,18 @@ import { i18n } from "@/lib/i18n-config";
 import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const blogUrls = (
+  const analysesIds = (
     await listAnalyses(1, 10000, { group: process.env.NEXT_PUBLIC_ROOT_DOMAIN })
-  ).map(
-    (blog) =>
-      `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/blog/${i18n.defaultLocale}/${blog.analysisId}`,
-  );
+  ).map((analysis) => analysis.analysisId);
 
   return [
-    {
-      url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/blog/${i18n.defaultLocale}`,
-    },
-    ...blogUrls.map((url) => ({ url })),
+    ...i18n.locales.flatMap((locale) => [
+      {
+        url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/blog/${locale}`,
+      },
+      ...analysesIds.map((analysisId) => ({
+        url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/blog/${locale}/${analysisId}`,
+      })),
+    ]),
   ];
 }
