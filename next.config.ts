ae463d6e062db8fcd30bf3ce159d0724c17ffe9a
nohsueh <nohsueh@qq.com>
2025-04-11 23:37:59 +0800

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath:
+    process.env.NEXT_PUBLIC_BASE_PATH === "/"
+      ? ""
+      : process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     remotePatterns: [
       {
