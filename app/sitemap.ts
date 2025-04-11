bbdc0f4b433bed181f8acb14a1b377316e43423d
nohsueh <nohsueh@qq.com>
2025-04-12 01:15:57 +0800

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,6 +4,7 @@ import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const BASE_URL = `https://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}${process.env.NEXT_PUBLIC_BASE_PATH || ""}`;
+  const CURRENT_DATE = new Date();
 
   const analysesIds = (
     await listAnalyses(1, 10000, { group: process.env.NEXT_PUBLIC_ROOT_DOMAIN })
@@ -14,11 +15,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     .flatMap((locale) => [
       {
         url: `${BASE_URL}/${locale}`,
-        priority: 1,
+        lastmod: CURRENT_DATE,
       },
       ...analysesIds.map((analysisId) => ({
         url: `${BASE_URL}/${locale}/${analysisId}`,
-        priority: 0.8,
+        lastmod: CURRENT_DATE,
       })),
     ]);
 }
