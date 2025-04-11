22fbf3a9afc4d7651a5b4563c2e1754ba79d0b32
nohsueh <nohsueh@qq.com>
2025-04-11 23:41:22 +0800

    update robots.ts, sitemap.ts and next.config.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,10 +11,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     .filter((locale) => locale === i18n.defaultLocale)
     .flatMap((locale) => [
       {
-        url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}${process.env.NEXT_PUBLIC_BASE_PATH === "/" ? "" : process.env.NEXT_PUBLIC_BASE_PATH}/${locale}`,
+        url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}${process.env.NEXT_PUBLIC_BASE_PATH || ""}/${locale}`,
       },
       ...analysesIds.map((analysisId) => ({
-        url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}${process.env.NEXT_PUBLIC_BASE_PATH === "/" ? "" : process.env.NEXT_PUBLIC_BASE_PATH}/${locale}/${analysisId}`,
+        url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}${process.env.NEXT_PUBLIC_BASE_PATH || ""}/${locale}/${analysisId}`,
       })),
     ]);
 }
