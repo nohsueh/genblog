6a39b440e020f7afd0cb98774773319a2c1f1505
nohsueh <nohsueh@qq.com>
2025-04-15 00:09:43 +0800

    update admin-dashboard.tsx

--- components/admin-dashboard.tsx
+++ components/admin-dashboard.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -204,12 +215,30 @@ export function AdminDashboard({
                         </span>
                       </Button>
                     </Link>
-                    <Button size="icon" variant="ghost">
-                      <Trash className="h-4 w-4" />
-                      <span className="sr-only">
-                        {dictionary.admin.dashboard.delete}
-                      </span>
-                    </Button>
+                    <AlertDialog>
+                      <AlertDialogTrigger>
+                        <Trash className="h-4 w-4" />
+                        <span className="sr-only">
+                          {dictionary.admin.dashboard.delete}
+                        </span>
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>
+                            Are you absolutely sure?
+                          </AlertDialogTitle>
+                          <AlertDialogDescription>
+                            This action cannot be undone. This will permanently
+                            delete your account and remove your data from our
+                            servers.
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel>Cancel</AlertDialogCancel>
+                          <AlertDialogAction>Continue</AlertDialogAction>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
                   </TableCell>
                 </TableRow>
               ))}
