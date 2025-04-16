e78388570f90d79b5503629911b5365aa76905fa
nohsueh <nohsueh@qq.com>
2025-04-17 02:25:59 +0800

    update layout.tsx and layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,24 +29,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION && (
-          <meta
-            name="google-site-verification"
-            content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION}
-          />
-        )}
-        {process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION && (
-          <script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ACCOUNT}`}
-            crossOrigin="anonymous"
-          ></script>
-        )}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `console.log('GOOGLE_ADSENSE:', '${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ACCOUNT}')`,
-          }}
+        <meta
+          name="google-site-verification"
+          content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION}
         />
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ACCOUNT}`}
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body>{children}</body>
     </html>
