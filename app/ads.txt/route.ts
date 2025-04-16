76d05d8335bfbf49ce16eb6c72db91e731dd6181
nohsueh <nohsueh@qq.com>
2025-04-17 00:21:37 +0800

    feat: create route.ts

---
+++ app/ads.txt/route.ts
@@ -0,0 +1,10 @@
+export async function GET() {
+  return new Response(
+    `google.com, ${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ACCOUNT}, DIRECT, f08c47fec0942fa0`,
+    {
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    }
+  );
+}
