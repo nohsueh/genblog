22da111ec6aeccaabd029e59c697b757704e4ffc
nohsueh <nohsueh@qq.com>
2025-04-16 20:37:52 +0800

    update site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export function SiteFooter() {
   return (
     <footer className="border-t py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
-        <div className="flex-col items-center gap-4 md:h-16 md:flex-row">
+        <div className="flex flex-col items-center gap-4 md:h-16 md:flex-row">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             © {new Date().getFullYear()} {process.env.NEXT_PUBLIC_APP_NAME}
           </p>
-          <div className="flex-row">
+          <div className="flex flex-row">
             <Link
               href={"https://github.com/nohsueh/blog-builder"}
               target="_blank"
