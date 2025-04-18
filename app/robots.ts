1e5e42c508bd2beb2b99c323e0c246bc51974c2e
nohsueh <nohsueh@qq.com>
2025-04-19 01:19:29 +0800

    update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,17 @@
 import type { MetadataRoute } from "next";
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
-  const BASE_URL = `https://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}${process.env.NEXT_PUBLIC_BASE_PATH || ""}`;
+  const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
+  const BASE_URL = `https://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}${BASE_PATH}`;
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: ["*"],
+        allow: ["/"],
+        disallow: ["/_next"],
+      },
+    ],
     sitemap: [`${BASE_URL}/sitemap.xml`],
   };
 }
