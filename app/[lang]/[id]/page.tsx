44b85554ba3a3115aab2a033dd9992fac83c56f8
nohsueh <nohsueh@qq.com>
2025-04-26 02:37:47 +0800

    update page.tsx

--- app/[lang]/[id]/page.tsx
+++ app/[lang]/[id]/page.tsx
@@ -48,7 +48,10 @@ export async function generateMetadata({
     .split("\n")
     .map((line) => line.trim())
     .filter((line) => line !== "");
-  const title = contentLines[0].replace(/^#+\s*/, "");
+  const title =
+    contentLines[0].replace(/^#+\s*/, "") +
+    " | " +
+    process.env.NEXT_PUBLIC_APP_NAME;
   const description = contentLines[1];
   const images = post.analysis.image;
 
