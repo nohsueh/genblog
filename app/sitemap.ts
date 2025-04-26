05cbcbc687a75f9507773f6a78ea177b52476253
nohsueh <nohsueh@qq.com>
2025-04-27 03:03:59 +0800

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,10 +6,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
   const BASE_URL = `https://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}${BASE_PATH}`;
   const CURRENT_DATE = new Date();
-
-  const analysesIds = (
-    await listAnalysesIds(1, 10000, { group: BASE_URL })
-  ).map((analysis) => analysis.analysisId);
+  const analyses = await listAnalysesIds(1, 10000, { group: BASE_URL });
 
   return i18n.locales
     .filter((locale) => locale === i18n.defaultLocale)
@@ -18,9 +15,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         url: `${BASE_URL}/${locale}`,
         lastModified: CURRENT_DATE,
       },
-      ...analysesIds.map((analysisId) => ({
-        url: `${BASE_URL}/${locale}/${analysisId}`,
-        lastModified: CURRENT_DATE,
+      ...analyses.map((analysis) => ({
+        url: `${BASE_URL}/${locale}/${analysis.analysisId}`,
+        lastModified: analysis.updatedAt,
       })),
     ]);
 }
