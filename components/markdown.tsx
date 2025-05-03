46e0c9ba7d8d262316f961f3ba1a4a7e7d1bc06d
nohsueh <nohsueh@qq.com>
2025-05-03 12:16:29 +0800

    update markdown.tsx

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,4 +1,6 @@
 import "prismjs";
+import "prismjs/components/prism-clike.js";
+import "prismjs/components/prism-markup-templating.js";
 import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import "prismjs/plugins/toolbar/prism-toolbar.css";
