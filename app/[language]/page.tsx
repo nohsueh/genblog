fffc9aa95fa7dba6b52e8b6452ad72a03e98d622
nohsueh <nohsueh@qq.com>
2025-05-14 13:34:32 +0800

    update page.tsx

--- app/[language]/page.tsx
+++ app/[language]/page.tsx
@@ -42,7 +42,7 @@ export default async function HomePage({
           />
         )}
         {getAppType() === "directory" && (
-          
+          <></>
         )}
       </main>
       <SiteFooter />
