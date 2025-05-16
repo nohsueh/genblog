31be20fad41f606e26bd1f61528f07b106c8fc43
nohsueh <nohsueh@qq.com>
2025-05-16 23:53:30 +0800

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -34,11 +34,9 @@ export default async function sitemap({
     {
       url: `${getBaseUrl()}/${locale}`,
     },
-    ...Array.from({ length: totalPage })
-      .slice(1)
-      .map((_, i) => ({
-        url: `${getBaseUrl()}/${locale}/page/${i + 1}`,
-      })),
+    ...Array.from({ length: totalPage }).map((_, i) => ({
+      url: `${getBaseUrl()}/${locale}/page/${i + 1}`,
+    })),
     ...analyses.map((analysis) => ({
       url: `${getBaseUrl()}/${locale}/${analysis.analysisId}/${encodeURIComponent(analysis.jsonContent?.slug || "")}`,
     })),
