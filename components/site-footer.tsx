b7e77dc0e62d6bbae467a57f87e3ab0fb2382ea3
nohsueh <nohsueh@qq.com>
2025-05-18 23:07:52 +0800

    update site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -27,7 +27,7 @@ export function SiteFooter() {
     <footer className="border-t bg-background py-8 md:py-12">
       <div className="container flex flex-col gap-8">
         {resourceGroups.length > 0 && (
-          <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+          <div className="grid grid-cols-2 gap-4 break-all sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
             {resourceGroups.map((group, index) => (
               <div key={index} className="flex flex-col space-y-3">
                 <h3 className="text-sm font-medium text-foreground">
